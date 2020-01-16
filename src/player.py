947321ee46a4f090e27483e214a9d15f35dd8093
bodhibrdr <julie.gumerman@gmail.com>
2020-01-16 16:33:18 -0700

    more code clean-up

--- src/player.py
+++ src/player.py
@@ -11,19 +11,17 @@ class Player:
         return f"Your location is the: {self.location}"
 
     def pick_up(self, the_item):
-        self.the_item = the_item
         for item in self.location.items:
-            if self.the_item in item.name:
+            if the_item in item.name:
                 self.items.append(item)
                 self.location.items.remove(item)
                 print(f"You have picked up {item.name}")
-        else:
-            print("That item is not in your current location.")
+            else:
+                print("That item is not in your current location.")
     
     def put_down(self, the_item):
-        self.the_item = the_item
         for item in self.items:
-            if self.the_item in item.name:
+            if the_item in item.name:
                 self.location.items.append(item)
                 self.items.remove(item)
                 print(f"You have put down {item.name}.")
